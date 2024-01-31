5ddeb9e28c91524804b6d4ebe8ae9cfe8e797d29
Turpal <39523349+turpalho@users.noreply.github.com>
2024-01-31 03:06:48 +0100

    Add files via upload

--- src/js/main.js
+++ src/js/main.js
@@ -4,18 +4,23 @@ mainButton.text = "ПРОДОЛЖИТЬ";
 mainButton.enable();
 mainButton.show();
 
-mainButton.onClick(function(){
-    var firstName = document.getElementById('first_name').value;
-    var lastName = document.getElementById('last_name').value;
-    var middleName = document.getElementById('middle_name').value;
+// mainButton.onClick(function(){
+//     var firstName = document.getElementById('first_name').value;
+//     var lastName = document.getElementById('last_name').value;
+//     var middleName = document.getElementById('middle_name').value;
 
-    let fio = {
-        name: firstName,
-        surname: lastName,
-        middleName: middleName
-    }
-    window.Telegram.WebApp.sendData(fio);
-})
+//     let fio = {
+//         name: firstName,
+//         surname: lastName,
+//         middleName: middleName
+//     }
+//     window.Telegram.WebApp.sendData("firstName");
+// })
+
+Telegram.WebApp.onEvent('mainButtonClicked', function(){
+    tg.sendData("some string that we need to send"); 
+    //при клике на основную кнопку отправляем данные в строковом виде
+ });
 
 // let BackButton = window.Telegram.WebApp.BackButton;
 // BackButton.hide();
@@ -56,5 +61,5 @@ function handleRegistration() {
         surname: lastName,
         middleName: middleName
     }
-    window.Telegram.WebApp.sendData(firstName);
+    window.Telegram.WebApp.sendData("firstName");
 }
